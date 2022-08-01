05684e25103852e36c08b734f1dded1971ad637d
sulaimanorsho@gmail.com <sulaimanorsho@gmail.com>
2022-08-01 16:54:29 +0300

    added top rated employees to homepage index

--- LO.CD.Web/Controllers/HomeController.cs
+++ LO.CD.Web/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using LO.CD.Entities;
+﻿using AutoMapper;
+using LO.CD.Entities;
+using LO.CD.Web.Data;
 using LO.CD.Web.Models;
+using LO.CD.Web.Models.Employees;
+using LO.CD.Web.Models.Home;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace LO.CD.Web.Controllers
@@ -8,16 +13,36 @@ namespace LO.CD.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IMapper mapper)
         {
             _logger = logger;
+            _context = context;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            
-            return View();
+
+            var HomePageVM = new HomePageViewModel();
+
+            HomePageVM.TopRatedEmloyees = await GetTopRatedEmployees();
+
+            return View(HomePageVM);
+        }
+
+        private async Task<List<EmployeesListViewModel>> GetTopRatedEmployees()
+        {
+            var top3ratedEmployees = await _context
+                                                          .Employees
+                                                          .OrderByDescending(e => e.Rating)
+                                                          .Take(3)
+                                                          .ToListAsync();
+
+            var top3ratedEmployeesVMs = _mapper.Map<List<Employee>, List<EmployeesListViewModel>>(top3ratedEmployees);
+            return top3ratedEmployeesVMs;
         }
 
         public IActionResult Privacy()
---
+++ LO.CD.Web/Models/Home/HomePageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LO.CD.Web.Models.Customers;
+using LO.CD.Web.Models.Employees;
+
+namespace LO.CD.Web.Models.Home
+{
+    public class HomePageViewModel
+    {
+        public HomePageViewModel()
+        {
+            var TopRatedEmloyees = new List<EmployeesListViewModel>();
+        }
+
+        public List<EmployeesListViewModel> TopRatedEmloyees { get; set; }
+    }
+}
